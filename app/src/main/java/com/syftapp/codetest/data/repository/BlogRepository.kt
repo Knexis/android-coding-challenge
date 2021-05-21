d7eb8f1746cafacb99b19a5a94c0a012c3ea32f9
Nana Kwame Nyantakyi <nanakwamnyantakyi@gmail.com>
2021-05-21 17:14:23 +0000

    Feature update: Updated fetchData to load data from remote source if loadMore is true in BlogRepository.kt

--- app/src/main/java/com/syftapp/codetest/data/repository/BlogRepository.kt
+++ app/src/main/java/com/syftapp/codetest/data/repository/BlogRepository.kt
@@ -12,6 +12,10 @@ import io.reactivex.Maybe
 import io.reactivex.Single
 import org.koin.core.KoinComponent
 
+/**
+ * Repository for handling the retrieval and saving of [User]s, [Comment]s and [Post]s
+ * remotely [blogApi] and locally [userDao], [commentDao], [postDao]
+ */
 class BlogRepository(
     private val postDao: PostDao,
     private val commentDao: CommentDao,
@@ -19,6 +23,12 @@ class BlogRepository(
     private val blogApi: BlogApi
 ) : KoinComponent, BlogDataProvider {
 
+    /**
+     * Handles:
+     * Fetching [User]s locally [userDao]
+     * Fetching [User]s remotely[blogApi]
+     * Saving   [User]s retrieved from [blogApi] into the database using[userDao]
+     */
     override fun getUsers(): Single<List<User>> {
         return fetchData(
             local = { userDao.getAll() },
@@ -27,6 +37,12 @@ class BlogRepository(
         )
     }
 
+    /**
+     * Handles:
+     * Fetching [Comment]s locally [commentDao]
+     * Fetching [Comment]s remotely[blogApi]
+     * Saving   [Comment]s retrieved from [blogApi] into the database using[commentDao]
+     */
     override fun getComments(): Single<List<Comment>> {
         return fetchData(
             local = { commentDao.getAll() },
@@ -35,34 +51,57 @@ class BlogRepository(
         )
     }
 
-    override fun getPosts(): Single<List<Post>> {
+    /**
+     * Handles:
+     * Fetching [Post]s locally [postDao]
+     * Fetching [Post]s remotely[blogApi]
+     * Saving   [Post]s retrieved from [blogApi] into the database using[postDao]
+     */
+    override fun getPosts(page: Int, limit: Int, loadMore: Boolean): Single<List<Post>> {
         return fetchData(
             local = { postDao.getAll() },
-            remote = { blogApi.getPosts() },
-            insert = { value -> postDao.insertAll(*value.toTypedArray()) }
+            remote = { blogApi.getPosts(page, limit) },
+            insert = { value -> postDao.insertAll(*value.toTypedArray()) },
+            loadMore = loadMore
         )
     }
 
+    /**
+     * Handles getting a single post from database given [postId]
+     * @param postId id of the post
+     * @return [Post] as a [Maybe]
+     */
     fun getPost(postId: Int): Maybe<Post> {
         return postDao.get(postId)
     }
 
+    /**
+     * Handles fetch data locally or remotely and saves new data from remote into database (locally)
+     * @param local local data source
+     * @param remote remote data source
+     * @param insert remote data source
+     * @param loadMore is false by default, set this to true to load data from [remote]
+     * @return [List] of data as [Single]
+     */
     private fun <T> fetchData(
         local: () -> Single<List<T>>,
         remote: () -> Single<List<T>>,
-        insert: (insertValue: List<T>) -> Completable
+        insert: (insertValue: List<T>) -> Completable,
+        loadMore: Boolean = false
     ): Single<List<T>> {
 
         return local.invoke()
             .flatMap {
-                if (it.isNotEmpty()) {
-                    Single.just(it)
-                } else {
+                //If there are no posts in the db or this was triggered by a loadmore
+                // action go ahead and load the data from remote data source
+                if(it.isEmpty() || loadMore){
                     remote.invoke()
                         .map { value ->
                             insert.invoke(value).subscribe();
                             value
                         }
+                }else{
+                    Single.just(it)
                 }
             }
     }
