155ae4ef52bfe59436c5c7e41f2300d4c5dfc498
Nana Kwame Nyantakyi <nanakwamnyantakyi@gmail.com>
2021-05-21 17:08:12 +0000

    Feature update: Added lastPage property to FinishedLoading class to track when there is no new data to be retrieved from the remote datasource.

--- app/src/main/java/com/syftapp/codetest/posts/PostScreenState.kt
+++ app/src/main/java/com/syftapp/codetest/posts/PostScreenState.kt
@@ -7,5 +7,5 @@ sealed class PostScreenState {
     class DataAvailable(val posts: List<Post>) : PostScreenState()
     class PostSelected(val post: Post) : PostScreenState()
     class Error(val error: Throwable) : PostScreenState()
-    object FinishedLoading : PostScreenState()
+    class FinishedLoading(val lastPage: Boolean) : PostScreenState()
 }
