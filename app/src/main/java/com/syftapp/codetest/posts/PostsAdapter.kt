f05b19570b0807a0acc69a6ae35db787059a21f2
Nana Kwame Nyantakyi <nanakwamnyantakyi@gmail.com>
2021-05-21 17:27:00 +0000

    Feature update: Added updateList function to add new posts to the list without resetting the entire screen. Added isEmpty to check if data is empty

--- app/src/main/java/com/syftapp/codetest/posts/PostsAdapter.kt
+++ app/src/main/java/com/syftapp/codetest/posts/PostsAdapter.kt
@@ -3,13 +3,18 @@ package com.syftapp.codetest.posts
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.syftapp.codetest.R
 import com.syftapp.codetest.data.model.domain.Post
 import kotlinx.android.synthetic.main.view_post_list_item.view.*
 
+/**
+ * RecyclerView Adapter for [Post]s
+ * Handles Display and update of [data] on posts [RecyclerView]
+ */
 class PostsAdapter(
-    private val data: List<Post>,
+    private val data: MutableList<Post>,
     private val presenter: PostsPresenter
 ) : RecyclerView.Adapter<PostViewHolder>() {
 
@@ -26,6 +31,25 @@ class PostsAdapter(
 
     override fun onBindViewHolder(holder: PostViewHolder, position: Int) {
         holder.bind(data.get(position))
+    }
+
+    /**
+     * Handles adding new [Post]s to the [data] list
+     * @param newPosts new posts to be added to the [data] list
+     */
+    fun updateList(newPosts: List<Post>) {
+        val start = data.size
+        data.addAll(newPosts)
+        val end = data.size
+        notifyItemRangeChanged(start, end)
+    }
+
+    /**
+     * Tells if the [data] list is empty
+     * @return true if there are no items in the [data] list
+     */
+    fun isEmpty(): Boolean{
+        return data.isEmpty()
     }
 }
 
